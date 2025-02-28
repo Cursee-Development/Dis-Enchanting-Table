33eccde8d742b9ab33ae23a812e8aa7eafa601b8
jason13official <jason13official@gmail.com>
2025-02-28 13:57:33 -0500

    block entity and container base

--- fabric/src/main/java/com/cursee/disenchanting_table/core/world/block/entity/FabricDisEnchantingBE.java
+++ fabric/src/main/java/com/cursee/disenchanting_table/core/world/block/entity/FabricDisEnchantingBE.java
@@ -1,13 +1,29 @@
 package com.cursee.disenchanting_table.core.world.block.entity;
 
 import com.cursee.disenchanting_table.core.registry.FabricBlockEntities;
+import com.cursee.disenchanting_table.core.world.inventory.AutoDisEnchantingMenu;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 
-public class FabricDisEnchantingBE extends BlockEntity {
+public class FabricDisEnchantingBE extends BlockEntity implements MenuProvider, IContainer {
+
+    private int progress = 0;
+    private final ContainerData containerData;
+    private final NonNullList<ItemStack> inventory = NonNullList.withSize(3, ItemStack.EMPTY);
 
     public FabricDisEnchantingBE(BlockPos pos, BlockState state) {
         this(FabricBlockEntities.DISENCHANTING_TABLE, pos, state);
@@ -15,9 +31,57 @@ public class FabricDisEnchantingBE extends BlockEntity {
 
     public FabricDisEnchantingBE(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
         super(type, pos, blockState);
+        containerData = new DisenchantingTableContainerData();
     }
 
     public void doTick(Level level, BlockPos pos, BlockState state, BlockEntity blockEntity) {
 
+    }
+
+    @Override
+    public NonNullList<ItemStack> getItems() {
+        return inventory;
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag data) {
+        ContainerHelper.saveAllItems(data, inventory);
+        data.putInt("progress", this.progress);
+        super.saveAdditional(data);
+    }
+
+    @Override
+    public void load(CompoundTag data) {
+        super.load(data);
+        ContainerHelper.loadAllItems(data, inventory);
+        this.progress = data.getInt("progress");
+    }
+
+    @Override
+    public Component getDisplayName() {
+        return Component.translatable("block.disenchanting_table.disenchanting_table");
+    }
+
+    @Override
+    public @Nullable AbstractContainerMenu createMenu(int i, Inventory inventory, Player player) {
+        return new AutoDisEnchantingMenu(i, inventory, this, containerData);
+    }
+
+    public class DisenchantingTableContainerData implements ContainerData {
+
+        @Override
+        public int get(int index) {
+            return index == 0 ? progress : 0;
+        }
+
+        @Override
+        public void set(int index, int value) {
+            if (index == 0) progress = value;
+        }
+
+        @Override
+        public int getCount() {
+            return 1;
+        }
     }
 }
--- forge/src/main/java/com/cursee/disenchanting_table/core/world/block/entity/ForgeDisEnchantingBE.java
+++ forge/src/main/java/com/cursee/disenchanting_table/core/world/block/entity/ForgeDisEnchantingBE.java
@@ -1,13 +1,55 @@
 package com.cursee.disenchanting_table.core.world.block.entity;
 
+import com.cursee.disenchanting_table.core.CommonConfigValues;
 import com.cursee.disenchanting_table.core.registry.ForgeBlockEntities;
+import com.cursee.disenchanting_table.core.util.DisenchantmentHelper;
+import com.cursee.disenchanting_table.core.world.block.DisEnchantingTableBlock;
+import com.cursee.disenchanting_table.core.world.inventory.AutoDisEnchantingMenu;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.Connection;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.Container;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class ForgeDisEnchantingBE extends BlockEntity {
+import java.util.Map;
+
+public class ForgeDisEnchantingBE extends BlockEntity implements MenuProvider, Container {
+    private ItemStackHandler itemHandler = new DisenchantingTableItemStackHandler();
+    private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
+    private final Map<Direction, LazyOptional<WrappedHandler>> directionWrappedHandlerMap =
+            new InventoryDirectionWrapper(itemHandler,
+                    new InventoryDirectionEntry(Direction.DOWN, 2, false),
+                    new InventoryDirectionEntry(Direction.NORTH, 1, CommonConfigValues.automatic_disenchanting),
+                    new InventoryDirectionEntry(Direction.SOUTH, 1, CommonConfigValues.automatic_disenchanting),
+                    new InventoryDirectionEntry(Direction.EAST, 1, CommonConfigValues.automatic_disenchanting),
+                    new InventoryDirectionEntry(Direction.WEST, 1, CommonConfigValues.automatic_disenchanting),
+                    new InventoryDirectionEntry(Direction.UP, 0, CommonConfigValues.automatic_disenchanting)).directionsMap;
+
+    private int progress = 0;
+    private final ContainerData containerData;
 
     public ForgeDisEnchantingBE(BlockPos pos, BlockState state) {
         this(ForgeBlockEntities.DISENCHANTING_TABLE, pos, state);
@@ -15,9 +57,174 @@ public class ForgeDisEnchantingBE extends BlockEntity {
 
     public ForgeDisEnchantingBE(BlockEntityType<?> pType, BlockPos pPos, BlockState pBlockState) {
         super(pType, pPos, pBlockState);
+        containerData = new DisenchantingTableContainerData();
     }
 
     public void doTick(Level level, BlockPos pos, BlockState state, BlockEntity blockEntity) {
         
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag data) {
+        data.put("inventory", itemHandler.serializeNBT());
+        data.putInt("progress", this.progress);
+        super.saveAdditional(data);
+    }
+
+    @Override
+    public void load(CompoundTag data) {
+        super.load(data);
+        itemHandler.deserializeNBT(data.getCompound("inventory"));
+        this.progress = data.getInt("progress");
+    }
+
+    @Override
+    public @Nullable AbstractContainerMenu createMenu(int pContainerId, Inventory pPlayerInventory, Player pPlayer) {
+        return new AutoDisEnchantingMenu(pContainerId, pPlayerInventory, this, this.containerData);
+    }
+
+    @Override
+    public Component getDisplayName() {
+        return Component.translatable("block.disenchanting_table.disenchanting_table");
+    }
+
+    @Override
+    public Packet<ClientGamePacketListener> getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
+    }
+
+    @Override
+    public @NotNull CompoundTag getUpdateTag() {
+        return saveWithoutMetadata();
+    }
+
+    @Override
+    public void onDataPacket(Connection net, ClientboundBlockEntityDataPacket pkt) {
+        super.onDataPacket(net, pkt);
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        lazyItemHandler = LazyOptional.of(() -> itemHandler);
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+        lazyItemHandler.invalidate();
+    }
+
+    @Override
+    public int getContainerSize() {
+        return 3;
+    } // container
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public ItemStack getItem(int pSlot) {
+        return itemHandler.getStackInSlot(pSlot);
+    }
+
+    @Override
+    public ItemStack removeItem(int pSlot, int pAmount) {
+        return itemHandler.extractItem(pSlot, pAmount, false);
+    }
+
+    @Override
+    public ItemStack removeItemNoUpdate(int pSlot) {
+        return itemHandler.extractItem(pSlot, itemHandler.getStackInSlot(pSlot).getCount(), true);
+    }
+
+    @Override
+    public void setItem(int pSlot, ItemStack pStack) {
+        itemHandler.setStackInSlot(pSlot, pStack);
+    }
+
+    @Override
+    public boolean stillValid(Player pPlayer) {
+        return true;
+    }
+
+    @Override
+    public void clearContent() {
+        for (int i = 0; i < itemHandler.getSlots(); i++) {
+            if (!itemHandler.getStackInSlot(i).isEmpty()) {
+                setItem(i, ItemStack.EMPTY);
+            }
+        }
+    } // container
+
+    @Override
+    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if(cap == ForgeCapabilities.ITEM_HANDLER) {
+            if(side == null) {
+                return lazyItemHandler.cast();
+            }
+
+            if(directionWrappedHandlerMap.containsKey(side)) {
+                Direction localDir = getBlockState().getValue(DisEnchantingTableBlock.FACING);
+
+                if(side == Direction.DOWN ||side == Direction.UP) {
+                    return directionWrappedHandlerMap.get(side).cast();
+                }
+
+                return switch (localDir) {
+                    default -> directionWrappedHandlerMap.get(side.getOpposite()).cast();
+                    case EAST -> directionWrappedHandlerMap.get(side.getClockWise()).cast();
+                    case SOUTH -> directionWrappedHandlerMap.get(side).cast();
+                    case WEST -> directionWrappedHandlerMap.get(side.getCounterClockWise()).cast();
+                };
+            }
+        }
+
+        return super.getCapability(cap, side);
+    }
+
+    public class DisenchantingTableContainerData implements ContainerData {
+
+        @Override
+        public int get(int index) {
+            return index == 0 ? progress : 0;
+        }
+
+        @Override
+        public void set(int index, int value) {
+            if (index == 0) progress = value;
+        }
+
+        @Override
+        public int getCount() {
+            return 1;
+        }
+    }
+
+    public class DisenchantingTableItemStackHandler extends ItemStackHandler {
+
+        public DisenchantingTableItemStackHandler() {
+            super(3); // todo fix magic value
+        }
+
+        @Override
+        protected void onContentsChanged(int slot) {
+            ForgeDisEnchantingBE.this.setChanged();
+            if(level != null && !level.isClientSide()) {
+                level.sendBlockUpdated(getBlockPos(), getBlockState(), getBlockState(), Block.UPDATE_ALL);
+            }
+        }
+
+        // used for can place item?
+        @Override
+        public boolean isItemValid(int slot, @NotNull ItemStack stack) {
+            return switch (slot) {
+                case 0 -> DisenchantmentHelper.canRemoveEnchantments(stack);
+                case 1 -> stack.is(Items.BOOK);
+                default -> false;
+            };
+        }
     }
 }
