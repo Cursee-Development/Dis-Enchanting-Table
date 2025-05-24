5d553550ab55637af715bbbda93564d7aa9fff7b
jason13official <jason13official@gmail.com>
2025-05-24 15:29:33 -0400

    register S2C packets on client in fabric

--- fabric/src/main/java/com/cursee/disenchanting_table/DisenchantingTableClientFabric.java
+++ fabric/src/main/java/com/cursee/disenchanting_table/DisenchantingTableClientFabric.java
@@ -1,8 +1,12 @@
 package com.cursee.disenchanting_table;
 
+import com.cursee.disenchanting_table.client.network.packet.FabricConfigSyncClientHandler;
+import com.cursee.disenchanting_table.client.network.packet.FabricItemSyncClientHandler;
 import com.cursee.disenchanting_table.client.renderer.blockentity.DisenchantingTableRenderer;
+import com.cursee.disenchanting_table.core.network.FabricNetwork;
 import com.cursee.disenchanting_table.core.registry.ModBlockEntities;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 
 public class DisenchantingTableClientFabric implements ClientModInitializer {
@@ -12,5 +16,8 @@ public class DisenchantingTableClientFabric implements ClientModInitializer {
         DisenchantingTableClient.init();
 
         BlockEntityRenderers.register(ModBlockEntities.DISENCHANTING_TABLE, DisenchantingTableRenderer::new);
+
+        ClientPlayNetworking.registerGlobalReceiver(FabricNetwork.Packets.CONFIG_SYNC_S2C, FabricConfigSyncClientHandler::receiveOnClient);
+        ClientPlayNetworking.registerGlobalReceiver(FabricNetwork.Packets.ITEM_SYNC_S2C, FabricItemSyncClientHandler::receiveOnClient);
     }
 }
